0c1d20ccb0dfa263f5762c2bbd5b80c8b97c7eb4
vahnchris79 <vahnchris79@gmail.com>
2025-10-19 19:44:43 +0900

    solve commit

--- bigdata_type1_exam_20240622.py
+++ bigdata_type1_exam_20240622.py
@@ -2,6 +2,7 @@
 # 기출문제 제8회 2024년 6월 22일 시행
 
 import pandas as pd
+import numpy as np
 path = "https://raw.githubusercontent.com/YoungjinBD/data/main/exam/"
 pd.set_option('display.width', 120)
 pd.set_option('display.max_row', 100)
@@ -23,12 +24,14 @@ result1 = df.groupby(['대륙'])['맥주소비량'].mean().idxmax()
 
 # 2) 이전 문제에서 구한 대륙에서 5번째로 맥주소비량이 많은 나라를 구하시오.
 df2 = df[df['대륙'] == result1].copy()
-result2 = df2.sort_values('맥주소비량', ascending=False)['국가'].values[5]
-# print(result2) # Colombia
+# result2 = df2.sort_values('맥주소비량', ascending=False)['국가'].values[5]
+result2 = df2.groupby('국가')['맥주소비량'].sum().sort_values(ascending=False).index[4]
+# print(result2) # Colombia -> Venezuela
 
 # 3) 이전 문제에서 구한 나라의 평균 맥주소비량을 구하시오.(소수점 첫째 자리에서 반올림)
-result3 = df[df['국가'] == result2].groupby(['국가'])['맥주소비량'].mean().reset_index()['맥주소비량'][0]
-# print(round(result3, 3)) # 251.491
+# result3 = df[df['국가'] == result2].groupby(['국가'])['맥주소비량'].mean().reset_index()['맥주소비량'][0]
+result3 = df.loc[df['국가'] == result2, '맥주소비량'].mean().round()
+# print(round(result3, 3)) # 251.491 -> 253.0
 
 # 2. 다음의 데이터는 국가별로 방문객 유형을 조사한 것이다.
 # 1) 관광객비율이 두 번째로 높은 나라의 '관광' 수를 구하시오.
@@ -40,20 +43,28 @@ result3 = df[df['국가'] == result2].groupby(['국가'])['맥주소비량'].mea
 # 풀이
 # 1) 관광객비율이 두 번째로 높은 나라의 '관광' 수를 구하시오.
 df = pd.read_csv(path + "8_1_2.csv")
+# print(df.shape) # (100, 6)
 # print(df.head())
-df['합계'] = df['관광'] + df['사무'] + df['공무'] + df['유학'] + df['기타']
-df['관광객비율'] = round(df['관광'] / df['합계'], 4)
-country = df.loc[df['관광객비율'] == sorted(df['관광객비율'], reverse=True)[1], '국가'].values[0]
-result1 = df.loc[df['국가'] == country, '관광'].values[1]
-# print(result1) # 7831
+
+# print(df['국가'].value_counts()) # 국가별 중복존재
+# 국가별 관광, 사무, 공무, 유학, 기타의 합계 계산
+df2 = df.groupby('국가')[['관광','사무','공무','유학','기타']].sum()
+# print(df2.shape) # (48, 5)
+df2['합계'] = df2.loc[:, '관광':'기타'].sum(axis=1)
+# print(df2.head())
+df2['관광객비율'] =  np.round(df2['관광'] / df2['합계'], 3)
+result1 = df2.sort_values('관광객비율', ascending=False)['관광'][1]
+# result1 = df2.sort_values(by='관광객비율', ascending=False).iloc[1, 1] 
+# print(result1) # 7831 -> 9039
 
 # 2) 관광 수가 두 번째로 높은 나라의 '공무' 수의 평균을 구하시오(소수점 첫째 자리에서 반올림)
-result2 = round(df.loc[df['국가'] == '이탈리아', '공무'].mean(), 1)
-# print(result2) # 647.3
+second = df2.sort_values(by='관광', ascending=False).index[1]
+result2 = round(df.loc[df['국가'] == second, '공무'].mean(), 0)
+print(result2) # 647.3 -> 494.0
 
 # 3) 이전에 구한 관광 수와 공무 수의 합계를 구하시오.
 result3 = result1 + result2
-# print(result3) # 8478.3
+# print(result3) # 8478.3 -> 9533.0
 
 # 3. CO(GT), NMHC(GT) 컬럼에 대해 Min-Max 스케일러는 실행하고, 스케일링된 CO(GT), NMHC(GT) 컬럼의 표준편차를 구하시오.
 # (소수점 셋째 자리에서 반올림)
@@ -63,6 +74,6 @@ df = pd.read_csv(path + "8_1_3.csv")
 
 temp = MinMaxScaler().fit_transform(df[['CO(GT)', 'NMHC(GT)']])
 df3 = pd.DataFrame(temp, columns=['CO(GT)', 'NMHC(GT)'])
-res_std1 = round(df3['CO(GT)'].std(ddof=1), 3)
-res_std2 = round(df3['NMHC(GT)'].std(ddof=1), 3)
-# print(res_std1, res_std2) # 0.367 0.146
+res_std1 = round(df3['CO(GT)'].std(ddof=1), 2)
+res_std2 = round(df3['NMHC(GT)'].std(ddof=1), 2)
+# print(res_std1, res_std2) # 0.367 0.146 -> 0.37, 0.15
---
+++ bigdata_type3_exam_20240622.py
@@ -0,0 +1,82 @@
+
+# 기출문제 제8회 2024년 6월 22일 시행
+
+import pandas as pd
+path = "https://raw.githubusercontent.com/YoungjinBD/data/main/exam/"
+pd.set_option('display.width', 120)
+pd.set_option('display.max_row', 100)
+
+# 작업형 제3유형
+# 문제1
+# 어느 회사에서 직원들의 업무 효율성을 높이기 위한 새로운 소프트웨어를 도입하였다.
+# 도입 전과 도입 후의 업무 처리 시간을 각각 측정하여 새로운 소프트웨어의 효과를 검증하고자 한다.
+# 1) 도입 전과 도입 후의 업무처리 시간의 평균과 표준편차를 구하시오.(소수점 둘째 자리까지 반올림)
+# 2) 도입 전후의 업무처리 시간 차이가 유의미한 지 부호 순위 검정을 실시하고, 검정통계량을 계산하시오.
+# (소수점 둘째 자리까지 반올림)
+# 3) p-value를 바탕으로 유의수준 5%에서 귀무가설의 기각/채택여부를 결정하시오.(p-value는 소수점 둘째 자리까지 반올림)
+
+# 풀이
+# 1) 도입 전과 도입 후의 업무처리 시간의 평균과 표준편차를 구하시오.(소수점 둘째 자리까지 반올림)
+df = pd.read_csv(path + "8_3_1.csv")
+# print(df.head())
+before_mean = df['before'].mean()
+before_std = df['before'].std()
+after_mean = df['after'].mean()
+after_std = df['after'].std()
+# print(round(before_mean, 2), round(before_std, 2), round(after_mean, 2), round(after_std, 2)) # 8.21 1.71 7.23 1.96
+
+# 2) 도입 전후의 업무처리 시간 차이가 유의미한 지 부호 순위 검정을 실시하고, 검정통계량을 계산하시오.
+# (소수점 둘째 자리까지 반올림)
+from scipy.stats import wilcoxon
+statistic, pvalue = wilcoxon(df['before'], df['after'])
+# print(round(statistic, 2), round(pvalue, 2)) # 72.0 0.0
+
+# 3) p-value를 바탕으로 유의수준 5%에서 귀무가설의 기각/채택여부를 결정하시오.(p-value는 소수점 둘째 자리까지 반올림)
+# print("채택" if round(pvalue, 2) > 0.05 else "기각") # 기각
+
+# 문제2
+# 어느 회사에서 직원들의 생산성에 영향을 미치는 요인이 무엇인지 확인하고자 한다. 100명의 직원들을 대상으로 생산성 점수,
+# 근무 시간, 연령, 그리고 경력을 조사하였다.
+train = pd.read_csv(path + "8_3_2_train.csv")
+test = pd.read_csv(path + "8_3_2_test.csv")
+# 1) 훈련데이터를 기준으로 생산성 점수(productivity)를 종속변수로 하고 근무 시간, 연령 그리고 경력을 독립변수로 하는 
+# 다중회귀 분석을 수행한 후, 회귀계수가 가장 높은 변수를 구하시오.(다중회귀모형 적합 시 절편 포함)
+# 2) 유의수준 5% 하에서 각 독립변수가 생산성에 미치는 영향이 통계적으로 유의미한 지 판단하고, 유의미한 변수 개수를 구하시오.
+# (p-value는 소수점 넷째 자리까지 반올림)
+# 3) 테스트 데이터로 모델의 성능을 평가하시오 (R^2 산출)
+
+# 풀이
+# 1) 훈련데이터를 기준으로 생산성 점수(productivity)를 종속변수로 하고 근무 시간, 연령 그리고 경력을 독립변수로 하는 
+# 다중회귀 분석을 수행한 후, 회귀계수가 가장 높은 변수를 구하시오.(다중회귀모형 적합 시 절편 포함)
+# print(train.shape, test.shape) # (80, 4) (20, 4)
+# print(train.head(), test.head(), sep="\n")
+import statsmodels.api as sm
+
+X = train[['hours', 'age', 'experience']]
+y = train['productivity']
+
+X = sm.add_constant(X) # 절편 포함
+
+model = sm.OLS(y, X).fit()
+# print(model.summary())
+
+# 회귀계수(coef) 확인(가장 높은 변수): hours(근무 시간)
+coefficients = model.params[1:]
+# print(coefficients.idxmax()) # hours
+
+# 2) 유의수준 5% 하에서 각 독립변수가 생산성에 미치는 영향이 통계적으로 유의미한 지 판단하고, 유의미한 변수 개수를 구하시오.
+# (p-value는 소수점 넷째 자리까지 반올림)
+pvalue = round(model.pvalues, 4)
+# print((pvalue < 0.05)[1:].sum()) # 3
+
+# 3) 테스트 데이터로 모델의 성능을 평가하시오 (R^2 산출)
+from sklearn.metrics import r2_score
+
+X_test = test[['hours', 'age', 'experience']]
+y_test = test['productivity']
+
+X_test = sm.add_constant(X_test)
+y_pred = model.predict(X_test)
+
+r2 = r2_score(y_test, y_pred)
+# print(r2) # 0.8036428205191006
